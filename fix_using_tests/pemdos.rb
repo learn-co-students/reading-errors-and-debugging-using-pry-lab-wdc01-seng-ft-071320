b44e63a17c973647f0c02a6d66061846c9983f8a
Jina Strickland <jina30@gmail.com>
2020-06-27 03:08:23 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,11 +2,11 @@
 require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    #binding.pry
-    #{}"#{10.times.s} #{string}"
+  if string[0] = "s"
+    letter = 10.times {|i| print "s"}
+    print "#{letter}string"
     #10 * "s" + string
   else
-    string
+    puts string
   end
 end
